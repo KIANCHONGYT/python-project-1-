e1b5496449f4a255fcf5c61055859573e905be2d
KIANCHONGYT <74892679+KIANCHONGYT@users.noreply.github.com>
2021-06-24 17:45:51 +0800

    Add files via upload

---
+++ index.py
@@ -0,0 +1,11 @@
+import turtlr as t
+import time
+t.color("red","yellow")
+t.speed(10)
+t.begin_fill()
+for_in range(500):
+    t.forward(200)
+    t.left(170)
+end_fill()
+time.sleep(1)
+t.mainloop()    
